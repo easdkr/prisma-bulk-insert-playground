f80a54125730908262d65b1db6dd2cc85bf717fd
June <easdkr@daum.net>
2024-06-09 20:28:02 +0900

    [feat] job 생성 부분 함수로 분리

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,24 @@ import { IterableJobQueue } from 'libs/job';
 import { pipe } from 'fp-ts/lib/function';
 import { chunksOf } from 'fp-ts/lib/ReadonlyArray';
 import { Logger } from '@nestjs/common';
+
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+// TODO: 위치 및 이름 변경, 파라미터 객체화
+const _createJobs = (ctx: any, name: string, data: any[], bulkSize: number) => {
+  return pipe(data, chunksOf(bulkSize), (chunks) =>
+    chunks.map((chunk) => async () => {
+      return await ctx.$parent[name].createMany({ data: chunk });
+    }),
+  );
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const _prisma = app.get(PrismaService);
   const logger = new Logger('prisma');
+
   const prisma = _prisma.$extends({
     name: 'prisma-bulk-insert',
     model: {
@@ -28,14 +39,10 @@ async function bootstrap() {
           const data = (args as any).data as any[];
           const concurrency = args.concurrency;
           const bulkSize = args.bulkSize;
+          const name = ctx.$name;
+
           console.time('job 생성');
-          const jobs = pipe(data, chunksOf(bulkSize), (chunks) =>
-            chunks.map((chunk) => async () => {
-              return await (ctx as any).$parent[ctx.$name as any].createMany({
-                data: chunk,
-              });
-            }),
-          );
+          const jobs = _createJobs(ctx, name, data, bulkSize);
           console.timeEnd('job 생성');
 
           console.time('전체 실행 시간');
@@ -47,13 +54,13 @@ async function bootstrap() {
   });
 
   await prisma.user.createBulk({
-    data: Array.from({ length: 10000000 }).map((_, i) => ({
+    data: Array.from({ length: 100 }).map((_, i) => ({
       email: `email ${i}`,
       name: `name ${i}`,
       password: `password ${i}`,
     })),
-    concurrency: 1,
-    bulkSize: 100000,
+    concurrency: 3,
+    bulkSize: 7,
   });
 
   logger.log('DONE', await prisma.user.count());
