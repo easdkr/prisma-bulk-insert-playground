804a780464fc9e364d1b5be3cade49757d40b20e
June <easdkr@daum.net>
2024-06-10 21:24:00 +0900

    [refactor] 분리된 함수를 사용하도록 job queue 변경

--- libs/job/iterable.job-queue.ts
+++ libs/job/iterable.job-queue.ts
@@ -2,7 +2,8 @@ import type { JobQueue } from './job-queue.interface';
 import { chunksOf, map } from 'fp-ts/lib/Array';
 import { pipe } from 'fp-ts/lib/function';
 import { flatten } from 'fp-ts/lib/ReadonlyArray';
-import { sequenceArray, type Task } from 'fp-ts/lib/Task';
+import { sequenceArray } from 'fp-ts/lib/Task';
+import { runSequentially, toArrayAsync } from 'libs/functional';
 
 export class IterableJobQueue<T> implements JobQueue<T> {
   private constructor(private readonly concurrency: number) {}
@@ -15,33 +16,8 @@ export class IterableJobQueue<T> implements JobQueue<T> {
       jobs,
       chunksOf(this.concurrency),
       map(sequenceArray),
-      this.runSequentially,
-      this.toArrayAsync,
+      runSequentially,
+      toArrayAsync,
     ).then(flatten);
-  }
-
-  /**
-   * 비동기 제너레이터를 사용하여 작업을 순차적으로 실행
-   */
-  private async *runSequentially(
-    tasks: Array<Task<ReadonlyArray<T>>>,
-  ): AsyncGenerator<ReadonlyArray<T>, void, unknown> {
-    for (const task of tasks) {
-      yield await task();
-    }
-  }
-
-  /**
-   * 비동기 이터러블을 배열로 변환
-   */
-  private async toArrayAsync<T>(iterable: AsyncIterable<T>) {
-    const result: T[] = [];
-
-    for await (const value of iterable) {
-      // console.log('value:', value);
-      result.push(value);
-    }
-
-    return result;
   }
 }
