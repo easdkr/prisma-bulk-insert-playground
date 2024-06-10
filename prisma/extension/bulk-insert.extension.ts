edf58d2c8fcf441635ed3a6027a4aa413948af70
June <easdkr@daum.net>
2024-06-10 21:33:51 +0900

    [refactor] insert 확장 주석 정리

--- prisma/extension/bulk-insert.extension.ts
+++ prisma/extension/bulk-insert.extension.ts
@@ -8,16 +8,18 @@ export const BulkInsertExtension = Prisma.defineExtension({
   name: 'bulk-insert',
   model: {
     $allModels: {
+      /**'
+       * 여러 개의 데이터를 한 번에 삽입하고 삽입된 데이터를 반환
+       * @param args.bulkSize 한 번에 처리할 데이터의 양 (동시에 삽입되는 데이터의 양 = bulkSize * concurrency)
+       * @param args.concurrency 병렬로 실행할 작업의 수 (동시에 시작하는 트랜잭션의 수, 커넥션 풀의 크기보다 작아야 함)
+       *
+       * ※ 주의: concurrency가 1 이상인 경우, 데이터의 삽입 순서가 보장되지 않음
+       *
+       */
       async createBulk<T, D>(
         this: T,
         args: Prisma.Exact<D, Prisma.Args<T, 'createManyAndReturn'>> & {
-          /**
-           * 병렬로 실행할 작업의 수 (동시에 시작하는 트랜잭션의 수, 커넥션 풀의 크기보다 작아야 함)
-           */
           concurrency?: number;
-          /**
-           * 한 번에 처리할 데이터의 양 (동시에 삽입되는 데이터의 양 = bulkSize * concurrency)
-           */
           bulkSize: number;
         },
       ) {
@@ -26,11 +28,11 @@ export const BulkInsertExtension = Prisma.defineExtension({
 
         const jobs = _createJobs(ctx, bulkSize, rest);
 
-        console.time('쿼리 실행 시간');
+        // console.time('쿼리 실행 시간');
         const res = await IterableJobQueue.of(concurrency ?? 1)
           .execute(jobs)
           .then(flatten); // createManyAndReturn의 반환값이 배열의 배열이므로 flatten을 사용하여 1차원 배열로 변환
-        console.timeEnd('쿼리 실행 시간');
+        // console.timeEnd('쿼리 실행 시간');
 
         return res;
       },
@@ -38,6 +40,14 @@ export const BulkInsertExtension = Prisma.defineExtension({
   },
 });
 
+/**
+ *
+ * @summary `bulkSize` 만큼의 데이터를 한 번에 삽입하는 작업 배열을 생성
+ * @param ctx 현제 모델의 컨텍스트
+ * @param bulkSize 한 번에 처리할 데이터의 양
+ * @param createManyAndReturnArgs createManyAndReturn 메서드의 인자
+ * @returns 작업 배열
+ */
 function _createJobs(
   ctx: any,
   bulkSize: number,
