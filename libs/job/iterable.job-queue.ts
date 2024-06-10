6bcdbee0895d0a4ac9525834c6730c15dab491d1
June <easdkr@daum.net>
2024-06-10 21:27:09 +0900

    [refactor] job-queue의 pipe의 각 단계에 설명 주석 추가

--- libs/job/iterable.job-queue.ts
+++ libs/job/iterable.job-queue.ts
@@ -14,10 +14,10 @@ export class IterableJobQueue<T> implements JobQueue<T> {
   public async execute(jobs: (() => Promise<T>)[]): Promise<ReadonlyArray<T>> {
     return await pipe(
       jobs,
-      chunksOf(this.concurrency),
-      map(sequenceArray),
-      runSequentially,
-      toArrayAsync,
-    ).then(flatten);
+      chunksOf(this.concurrency), // 작업을 동시성 수준에 따라 청크로 나눔
+      map(sequenceArray), // 청크를 순차적으로 실행할 수 있는 Task로 변환
+      runSequentially, // 각 청크 Task를 순차적으로 실행하고 결과를 방출
+      toArrayAsync, // AsyncIterable을 배열로 변환
+    ).then(flatten); // 각 청크 결과를 하나의 배열로 평탄화
   }
 }
